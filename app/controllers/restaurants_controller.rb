e543eba83d6368eb5c3d9b863dfe7bb37143216e
Emily Burns <emily5001@gmail.com>
2018-05-23 15:57:48 +0100

    updated the routes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,4 +5,16 @@ class RestaurantsController < ApplicationController
     @restaurants = Resturants.all
   end
 
+  def show
+    @reviews = @restaurant.reviews
+
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new()
+  end
+
+  def edit
+
+  end
+
+
 end
